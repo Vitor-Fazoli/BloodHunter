04b559a1e46d203a579079fff9cdd61017a2b497
Fazoli <86382456+Vitor-Fazoli@users.noreply.github.com>
2024-01-07 11:22:40 -0300

    Added rule of loots

---
+++ Common/GlobalNPCs/LootNPCs.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria;
+using BloodHunter.Content.Itens.Materials;
+
+namespace BloodHunter.Common.GlobalNPCs
+{
+    public class LootNPCs : GlobalNPC
+    {
+        public override void ModifyNPCLoot(NPC npc, NPCLoot npcLoot)
+        {
+            if (npc.type == NPCID.EyeofCthulhu)
+            {
+                foreach (var rule in npcLoot.Get())
+                {
+                    if (rule is DropBasedOnExpertMode dropBasedOnExpertMode && dropBasedOnExpertMode.ruleForNormalMode is OneFromOptionsNotScaledWithLuckDropRule oneFromOptionsDrop && oneFromOptionsDrop.dropIds.Contains(ItemID.BeeGun))
+                    {
+                        var original = oneFromOptionsDrop.dropIds.ToList();
+                        //original.Add(ModContent.ItemType<Cont.>());
+                        oneFromOptionsDrop.dropIds = [.. original];
+                    }
+                }
+            }
+
+            if (npc.type == NPCID.BloodNautilus)
+            {
+
+            }
+
+            if (npc.type == NPCID.WallofFlesh)
+            {
+                
+            }
+
+            if (npc.type == NPCID.Retinazer)
+            {
+            
+            }
+
+            if (npc.type == NPCID.Spazmatism)
+            {
+                
+            }
+
+            if (npc.type == NPCID.CultistBoss)
+            {
+            
+            }
+
+            if (npc.type == NPCID.DukeFishron)
+            {
+            
+            }
+
+            // 0.5% chance to drop Adamantite Fragment of any tipe of slime
+            if (npc.aiStyle == NPCAIStyleID.Slime)
+            {
+                npcLoot.Add(ItemDropRule.ByCondition(Condition.TimeDay.ToDropCondition(ShowItemDropInUI.Always), ModContent.ItemType<AdamantiteFragment>(), chanceDenominator: 20));
+            }
+        }
+    }
+}
---
+++ Content/Itens/Materials/AdamantiteFragment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace BloodHunter.Content.Itens
+{
+    public class AdamantiteFragment : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.rare = ItemRarityID.Lime;
+        }
+    }
+}
