014ed2adc7cdf44adb8157be08cbe9c20cec0adf
Fazoli <86382456+Vitor-Fazoli@users.noreply.github.com>
2024-01-16 12:41:24 -0300

    testing dust and sounds

--- Content/Items/Accessories/CursedScabbard.cs
+++ Content/Items/Accessories/CursedScabbard.cs
@@ -1,6 +1,7 @@
 ﻿using BloodHunter.Content.Projectiles;
 using Microsoft.Xna.Framework;
 using Terraria;
+using Terraria.Audio;
 using Terraria.DataStructures;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -27,7 +28,16 @@ namespace BloodHunter.Content.Items.Accessories
                 {
                     player.lifeRegenTime = 0;
                     player.AddBuff(ModContent.BuffType<Buffs.CursedSword>(), p.getBloodRate * 5);
+
+                    for (int i = 0; i < 50; i++)
+                    {
+                        Vector2 speed = Main.rand.NextVector2CircularEdge(1f, 1f);
+                        Dust d = Dust.NewDustPerfect(player.position + new Vector2(0, -30), DustID.ManaRegeneration, speed * 5, Scale: 1.5f);
+                        d.noGravity = true;
+                    }
+                    SoundEngine.PlaySound(SoundID.Item44, player.position + new Vector2(0, -30));
                     Projectile.NewProjectile(new EntitySource_TileBreak(2, 2), player.position + new Vector2(0, -30), Vector2.Zero, ModContent.ProjectileType<CursedSword>(), p.bloodMax2 / 2, 5);
+
                     p.bloodCurrent = 0;
                 }
             }
--- obj/Debug/net6.0/BloodHunter.AssemblyInfo.cs
+++ obj/Debug/net6.0/BloodHunter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloodHunter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+586c3e62c8c62d13e6257e741e8c55030edfe4d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a5f67a9528c6559d5d72f4df5739a0a9f33c6b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloodHunter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloodHunter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
